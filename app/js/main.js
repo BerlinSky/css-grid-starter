b769f66626730d12c71b6db15049f9851a5b0770
Berlin Sky <pedroyoung8989@gmail.com>
2018-05-23 08:04:14 -0700

    update validation

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,6 @@
 import { isDocReady, qs }from "./library/html-dom"
 import { simpleSearchContainerMarkup } from "./components/simple-search-widget"
 
-// import { lastNameMin, magicIdLength } from "./components/validation/validation-rules"
-// import { lastNameInput, magicIdInput } from "./components/validation/validator"
-
 import { validateSearchInputs } from "./components/validation/validate-engine"
 
 const addEventListeners = () => {
