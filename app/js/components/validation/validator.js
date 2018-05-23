4358f5b762d63cb3f49b67302d4b859fd7b17952
Berlin Sky <pedroyoung8989@gmail.com>
2018-05-23 07:49:27 -0700

    update validation

---
+++ app/js/components/validation/validator.js
@@ -0,0 +1,15 @@
+import { qs } from "../../library/html-dom"
+
+// Set up the input value: will be replaced Pick up the input value
+export const lastNameInput = () => {
+  const elem = qs("input[name='lasttName'].js-searchInput ");
+  return { min: elem.value.length }
+}
+
+export const magicIdInput = () => {
+  const elem = qs("input[name='magicName'].js-searchInput ");
+
+  console.log(elem.value.length);
+
+  return { length: elem.value.length };
+}
