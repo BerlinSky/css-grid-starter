64fb8e9b6a25de781ca5211d8a59e419b5173f4b
Berlin Sky <pedroyoung8989@gmail.com>
2018-06-01 16:12:30 -0700

    update lightbox

--- app/js/main.js
+++ app/js/main.js
@@ -6,10 +6,10 @@ const setupMiniLightbox = () => {
     'content': `
         <div class="lightboxContent js-lightboxContent">
           <img class="js-lightboxImage">
-          <a class="imageNavPanel imageNavPanel--left" href="/media/photo/364/11">
+          <a class="imageNavPanel imageNavPanel--left js-lbImage__prev" href="#">
             <i class="fa fa-chevron-circle-left fa-4x"></i>
           </a>
-          <a class="imageNavPanel imageNavPanel--right" href="/media/photo/367/11">
+          <a class="imageNavPanel imageNavPanel--right js-lbImage__next" href="#">
             <i class="fa fa-chevron-circle-right fa-4x"></i>
           </a>
           <div class="imageTitle">
@@ -30,18 +30,71 @@ const addImageSource = (imagePath) => {
 
 const activateImageTag = () => {
   const elemList = document.querySelectorAll('.js-lbImage');
-
   elemList.forEach(function(elem) {
     elem.addEventListener("click", function(e) {
       addImageSource(e.target.getAttribute("data-fullpath"));
+      // const t = getNextImageListItem(e.target);
+
+      // console.log('t ', t);
       showMiniLgithbox();
     });
   });
 }
 
-// const allImageList = () => {
-//   const imageList = document.querySelectorAll('.js-lbImage');
-// }
+const activateNextButton = () => {
+  const elem = document.querySelector('.js-lbImage__next');
+
+  elem.addEventListener('click', function() {
+    const imageElem = document.querySelector('.js-lightboxImage');
+
+    const nextImagePath = getNextImagePath(imageElem.getAttribute('src'));
+    console.log('nextImagePath', nextImagePath);
+
+    imageElem.setAttribute('src', nextImagePath);
+  })
+}
+
+const activatePrevButton = () => {
+  const elem = document.querySelector('.js-lbImage__prev');
+
+  elem.addEventListener('click', function() {
+    const imageElem = document.querySelector('.js-lightboxImage');
+    const nextImagePath = getPrevImagePath(imageElem.getAttribute('src'));
+    console.log('nextImagePath', nextImagePath);
+
+    imageElem.setAttribute('src', nextImagePath);
+  })
+}
+
+const getNextImagePath = (currentImagePath) => {
+  const imageList = document.querySelectorAll('.js-lbImage');
+
+  for (let i = 0; i < imageList.length; i++) {
+    const thisPath = imageList[i].getAttribute('data-fullPath');
+
+    if (thisPath === currentImagePath) {
+      const nextIndex = i < (imageList.length - 1) ? (i + 1) : 0;
+      const nextPath = imageList[nextIndex].getAttribute('data-fullPath');;
+
+      return nextPath;
+    }
+  }
+}
+
+const getPrevImagePath = (currentImagePath) => {
+  const imageList = document.querySelectorAll('.js-lbImage');
+
+  for (let i = 0; i < imageList.length; i++) {
+    const thisPath = imageList[i].getAttribute('data-fullPath');
+
+    if (thisPath === currentImagePath) {
+      const previousIndex = (i > 0) ? (i - 1) : (imageList.length - 1);
+      const nextPath = imageList[previousIndex].getAttribute('data-fullPath');;
+
+      return nextPath;
+    }
+  }
+}
 
 $(function () {
   initMiniLightbox();
@@ -49,4 +102,7 @@ $(function () {
   setupMiniLightbox();
 
   activateImageTag();
+
+  activatePrevButton();
+  activateNextButton();
 });
