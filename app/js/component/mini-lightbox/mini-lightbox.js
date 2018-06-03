b2e725281513fc769dfd317df0b7e55ccd9a43b3
Berlin Sky <pedroyoung8989@gmail.com>
2018-06-03 15:06:54 -0700

    update lightbox

--- app/js/component/mini-lightbox/mini-lightbox.js
+++ app/js/component/mini-lightbox/mini-lightbox.js
@@ -47,6 +47,14 @@ const createPopup = () => {
 }
 const createMinLightbox = () => {
   const wrapper = createOverlayWrapper();
+
+  const clodeButton = createCloseButton();
+  clodeButton.addEventListener("click", function() {
+    hideMiniLgithbox();
+  })
+  wrapper.appendChild(clodeButton);
+
+
   const baseLayer = createBaseLayer();
   wrapper.appendChild(baseLayer);
 
@@ -63,6 +71,46 @@ const escapeMe = () => {
       hideMiniLgithbox();
     }
   })
+}
+
+const createCloseButton = () => {
+  const elem = document.createElement('button');
+  const style = `
+    position: fixed;
+    width: 40px;
+    height: 40px;
+    transition: all .2s ease-in-out;
+    opacity: .8;
+    top: 0;
+    left: 0;
+  `;
+  elem.setAttribute('style', style);
+
+  const innerElem = document.createElement('div');
+  const innerStyle = `
+    display: -webkit-box;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+  `;
+  innerElem.setAttribute('style', innerStyle);
+  innerElem.innerHTML = createCloseIcon();;
+
+  elem.appendChild(innerElem);
+
+  return elem;
+}
+
+const createCloseIcon = () => {
+  const style = "width:24px;height:24px;fill:#333333;";
+
+  return `
+    <svg style=${style} version="1.1" viewBox="0 0 32 32" width="32" height="32" aria-hidden="false">
+      <path d="M25.33 8.55l-1.88-1.88-7.45 7.45-7.45-7.45-1.88 1.88 7.45 7.45-7.45 7.45 1.88 1.88 7.45-7.45 7.45 7.45 1.88-1.88-7.45-7.45z"></path>
+    </svg>
+  `;
 }
 
 export const initMiniLightbox = () => {
